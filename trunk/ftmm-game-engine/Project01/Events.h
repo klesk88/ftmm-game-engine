02eb9d4c5c5a344a717c18015a0151284628ac66
ermac988@gmail.com <ermac988@gmail.com@e7e6074e-f8f0-993b-5f30-e5e04b8333df>
2011-10-02 15:52:41 +0000

    events added

---
+++ trunk/ftmm-game-engine/Project01/Events.h
@@ -0,0 +1,46 @@
+#include "SDL.h"
+#include "SDL_opengl.h"
+#include <windows.h>
+
+class Event{//must be an interface for all the events
+
+public:	
+		double cycles;//@ param number of cycles
+		int start;
+		DWORD milliseconds;//@param  number of milliseconds
+		float tick;
+		
+		Event();
+		/*
+		@warning
+			time of life of the the event, before it die
+		*/
+		virtual void timeOfLife()=0;
+		/*
+		@warning
+			update the time of the object based
+			on the ticks of the cpu clock
+		*/
+		void updateCycles();
+		/*
+		@warning
+			update the time of the object based
+			on milliseconds		
+		*/
+		void updateTime();
+		/*
+		@warning
+			set the time variable in milliseconds
+		*/
+		void setInitialTime(int time);
+
+		/*
+		@warning
+			set the clock variable
+		*/
+		void setInitialClock(double clock);
+
+
+		
+
+};
