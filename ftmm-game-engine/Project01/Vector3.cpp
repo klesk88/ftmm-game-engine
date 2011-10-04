9fff30f0eecc2943b1a14b39b8c97544ec021c63
marcofuma89@gmail.com <marcofuma89@gmail.com@e7e6074e-f8f0-993b-5f30-e5e04b8333df>
2011-10-04 17:40:58 +0000

    finished Light class add default costructor in Vector3

--- ftmm-game-engine/Project01/Vector3.cpp
+++ ftmm-game-engine/Project01/Vector3.cpp
@@ -20,6 +20,13 @@ Quaternion 	getRotationTo (const Vector3 &dest, const Vector3 &fallbackAxis=Vect
 */
 #include "Vector3.h"
 
+Vector3::Vector3()
+{
+	x = 0.0f;
+	y = 0.0f;
+	z = 0.0f;
+}
+
 Vector3::Vector3(float xF, float yF, float zF)
 {
 	x=xF; y=yF; z = zF;
