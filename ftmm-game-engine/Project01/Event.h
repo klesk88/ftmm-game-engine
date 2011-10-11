140563b04aa3e2097277ee8a403d8c6a00956109
tilman.geishauser@gmail.com <tilman.geishauser@gmail.com@e7e6074e-f8f0-993b-5f30-e5e04b8333df>
2011-10-11 10:29:16 +0000

    Correctly included InputManagement

--- ftmm-game-engine/Project01/Event.h
+++ ftmm-game-engine/Project01/Event.h
@@ -1,43 +1,60 @@
+#ifndef __Event_h__
+#define __Event_h__
+
 #include "SDL.h"
 #include "SDL_opengl.h"
 #include <windows.h>
+#include <ctime>
+#include <vector>
 
 class Event{//must be an interface for all the events
 
 public:	
-		double cycles;//@ param number of cycles
-		int start;
-		DWORD milliseconds;//@param  number of milliseconds
-		float tick;
+		clock_t cycles;//@ param number of clock ticks
+		unsigned int start;
+		unsigned int milliseconds;//@param  number of milliseconds
+		clock_t tick;
 		
+
 		Event();
-		/*
-		@warning
-			time of life of the the event, before it die
-		*/
-		virtual void timeOfLife()=0;
 		/*
 		@warning
 			update the time of the object based
 			on the ticks of the cpu clock
 		*/
-		void updateCycles();
+		virtual void updateCycles();
 		/*
 		@warning
 			update the time of the object based
 			on milliseconds		
 		*/
-		void updateTime();
+		virtual void updateTime();
 		/*
 		@warning
 			set the time variable in milliseconds
 		*/
-		void setInitialTime(int time);
+		virtual void setInitialTime(unsigned int time);
 
 		/*
 		@warning
 			set the clock variable
 		*/
-		void setInitialClock(double clock);
+		virtual void setInitialClock(clock_t clo);
 
+		/*
+			set the priority of the event
+		*/
+		void setPriority(int);
+		int getPriority();
+		void setStartEndIndex(std::vector<Event*>::iterator start,std::vector<Event*>::iterator end);
+		std::vector<Event*>::iterator getStartIndex();
+		std::vector<Event*>::iterator getEndIndex();
+private:
+
+	int priority;
+
+	//start and end of the type of event inside the vector list
+	std::vector<Event*>::iterator started,end;
 };
+
+#endif
