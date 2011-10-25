f3a8c7ed93e6287641f8aff489447efe0cc5b568
tilman.geishauser@gmail.com <tilman.geishauser@gmail.com@e7e6074e-f8f0-993b-5f30-e5e04b8333df>
2011-10-25 17:37:00 +0000

    Including game loop in main project, some changes... not done, but everything runs as it did run before

--- ftmm-game-engine/Project01/EventHandler.h
+++ ftmm-game-engine/Project01/EventHandler.h
@@ -1,16 +1,28 @@
 #pragma once//include only one time on the compilation this file
 #ifndef __EventHandler_h__
 #define __EventHandler_h__
-#include <list>
+#include <vector>
 #include "Event.h"
+#include <typeinfo>
 
 
 class EventHandler{
 
 public:
-	std::list <Event*> list_of_events;//list containing all the game objects connected with this frame listener
+	const enum positions{Input,Ai,Physics,Sound};//positions in the vector
+	EventHandler();
+	~EventHandler();
+	void addEvents(std::vector<Event*> ev);
+	(std::vector <std::vector<Event*>>)* returnVectorOfEvents();
+	/*
+		update the TimeOfLife(tol) of all the events and, if the tol is euqal to zero
+		delete them from the vector
+	*/
+	void updateEvent();
 
-	void addEvents(Event* ev);
-	void removeEvents();
+private:
+
+	std::vector <std::vector<Event*>>* vector_of_events;//list containing all the events 
+
 };
 #endif
--- ftmm-game-engine/Project01/SceneManager.h
+++ ftmm-game-engine/Project01/SceneManager.h
@@ -13,27 +13,43 @@
 #include "Mesh.h"
 #include "RootTransformNode.h"
 #include "Camera.h"
+#include "FrameListener.h"
 
-class SceneManager{
+
+class SceneManager
+{
 
 	public:
-
+		
 		static SceneManager * getInstance ();
 		TransformNode * getRootTransformNode();
 		Camera * createCamera(const std::string  & stringName);
 		void renderScene();
-		~SceneManager(){delete this;}
+		~SceneManager(){/*delete this;*/}
 		//Texture function
 		int LoadGLTextures(char*, int);
 		GLuint getTexture(int);
-
+		/*	@Param base_fps: how many fps you want
+			@Param low_fps: minimum fps for having game logic at base fps speed
+		*/
+		void callGameLoop(bool,const int base_fps,const int low_fps);
+		void createFrameListener(FrameListener*);
+		/*
+			i set the game_is:run variable so i know if the game loop have to start or not.
+			after i call the method for call the game loop with the right parameters		
+		*/
+		void startEngine(bool,const int base_fps,const int low_fps);
+		std::list <FrameListener*> framelistener_list;
 	protected:
+
 		SceneManager();
 		TransformNode * root_ptr;
 		std::list <Camera*> camera_list;
+		
 
 	private:
 
+		bool gameLoop(const int base_fps,const int low_fps);
 		static SceneManager * p_instance;
 		/*texture*/
 		static const int size = 1;
@@ -41,6 +57,4 @@ class SceneManager{
 
 
 };
-
-
 #endif
