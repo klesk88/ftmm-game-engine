ce428c8d61d948e286d42acd6b8b1be2551b9f85
tilman.geishauser@gmail.com <tilman.geishauser@gmail.com@e7e6074e-f8f0-993b-5f30-e5e04b8333df>
2011-11-01 11:21:05 +0000

    sketched GameTime.h

---
+++ ftmm-game-engine/Project01/GameTime.h
@@ -0,0 +1,20 @@
+#ifndef __GameTime_h__
+#define __GameTime_h__
+
+
+class GameTime
+{
+public:
+
+	// The amount of elapsed game time since the last update.
+	// public TimeSpan ElapsedGameTime { get; set; }
+	Uint32 get_elapsedGameTime();
+
+	// Gets a value indicating that the game loop is taking longer than its TargetElapsedTime. In this case, the game loop can be considered to be running too slowly and should do something to "catch up."
+	bool IsRunningSlowly();
+
+	// The amount of game time since the start of the game.
+	Uint32 TotalGameTime();
+};
+
+#endif
