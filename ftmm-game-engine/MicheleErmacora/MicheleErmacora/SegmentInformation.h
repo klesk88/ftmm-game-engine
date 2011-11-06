48071ac41c256f5c5c8d712ea5884bbef10ad257
ermac988@gmail.com <ermac988@gmail.com@e7e6074e-f8f0-993b-5f30-e5e04b8333df>
2011-11-06 15:31:02 +0000

    update memorymanagement system

--- ftmm-game-engine/MicheleErmacora/MicheleErmacora/SegmentInformation.h
+++ ftmm-game-engine/MicheleErmacora/MicheleErmacora/SegmentInformation.h
@@ -1,13 +1,13 @@
-#include "EAllocationType.h";
+#include "EAllocationType.h"
 
 struct SegmentInformation{
 	
 	//information about the next position of the segment
-	SegmentInformation * next_free_segment;
+	unsigned int next_free_segment;
 	//type of the segment (physics, ai, ...)
 	EAllocationType allocation_type;
 	//the block is free or not?
-	EBlockType block_type; 
+	EIsAvailable is_available; 
 	//dimension of the segment
 	unsigned int dimension;
 };
