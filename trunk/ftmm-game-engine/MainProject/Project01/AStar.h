f685bc48bbde70a17624551d6f4b58ab48bf6088
tilman.geishauser@gmail.com <tilman.geishauser@gmail.com@e7e6074e-f8f0-993b-5f30-e5e04b8333df>
2011-11-29 15:56:33 +0000

    comments

--- trunk/ftmm-game-engine/MainProject/Project01/AStar.h
+++ trunk/ftmm-game-engine/MainProject/Project01/AStar.h
@@ -1,9 +1,21 @@
 #ifndef __AStar_H
 #define __AStar_H
 
+// dfg http://www.ai-blog.net/archives/000152.html
+// fg http://newsgroups.derkeiler.com/Archive/Rec/rec.games.roguelike.development/2008-12/msg00128.html
+
 class AStar
 {
 
 };
+
+
+struct Node
+{
+	int c; // cost of node + predecessors
+	int h; // heuristic estimate of distance to goal
+	int f; // sum of predecessors, self + heuristic
+};
+
 
 #endif
